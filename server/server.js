7cf20f40fbfa62d5ae99da7f485b64393bfe6045
Taha Ghous <tahaghous2002@gmail.com>
2025-09-30 22:29:10 +0500

    some changes

--- server/server.js
+++ server/server.js
@@ -4,16 +4,11 @@ import connectDB from "./db/connectDB.js";
 import adminRouter from "./routes/admin.route.js";
 import blogRouter from "./routes/blogs.route.js";
 
-//connet mongoDB
-
 await connectDB();
 const app = express();
 
-//Middlewares
 app.use(cors());
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Server is Working");
@@ -23,8 +18,6 @@ app.get("/", (req, res) => {
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at Port : ${PORT}`);
-});
+// ❌ REMOVE app.listen() for vercel
 
 export default app;

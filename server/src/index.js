d9de1bd83434cc03f711c43f3c6f229bd637f17d
Taha Ghous <tahaghous2002@gmail.com>
2025-09-30 21:49:35 +0500

    some changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,19 @@
+// import connectDB from "../db/connectDB.js";
+// import app from "./app.js";
+
+// await connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server is running at Port : ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("MONGO db Connection failed !", error);
+//   });
+
+// index.js
 import connectDB from "../db/connectDB.js";
 import app from "./app.js";
 
-await connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at Port : ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("MONGO db Connection failed !", error);
-  });
+await connectDB();
+export default app; // no app.listen
